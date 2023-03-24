4e00c295ae9b9beb23bc0709e84ed87a10774838
Saglara1 <lidzhieva@gmail.com>
2023-03-25 01:01:00 +0300

    1. во втором задании добавила целочисленное деление 2. во втором задании добавила вариант 2 по расчету секунд (ошибка, не могу найти) 3. добавила задание 3

--- lesson 1/lesson1.py
+++ lesson 1/lesson1.py
@@ -1,4 +1,6 @@
+# # #
 # Задание 1
+
 c = 10
 d = "привет"
 
@@ -14,14 +16,38 @@ print(id(e))
 print(id(f))
 print(id(j))
 
-# Задание 2
+# Задание 2 - вариант 1
+
 sec = input('Введите число секунд: ')
 
 if sec.isdigit():
-    mins = int(sec)/60
-    hours = mins/60
+    mins = int(sec)//60
+    hours = mins//60
     print('Часы:', hours)
     print('Минуты:', mins)
     print('Секунды:', int(sec))
 else:
     print('Надо было вводить число!')
+# # #
+
+# Задание 2 - вариант 2
+
+sec1 = input('Введите число секунд: ')
+
+if sec1.isdigit():
+    hours = int(sec1)//3600
+    sec2 = int(sec1) - 600*int(hours)
+    mins = int(sec2)//60
+    sec3 = int(sec1) - int(hours*3600) - int(mins*60)
+    print('Часы:', hours)
+    print('Минуты:', mins)
+    print('Секунды:', sec3)
+else:
+    print('Надо было вводить число!')
+# # #
+# Задание 3
+n = int(input("Введите число от 1 до 9: "))
+nn = n*10+n
+nnn = n*100 + nn
+sum = n+nn+nnn
+print(sum)
