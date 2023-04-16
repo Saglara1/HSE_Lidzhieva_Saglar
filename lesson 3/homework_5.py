5a0b343f290c12980797603d048fd24f27275cc2
Saglara1 <lidzhieva@gmail.com>
2023-04-16 14:03:14 +0300

    Кирилл, это итоговая версия

--- lesson 3/homework_5.py
+++ lesson 3/homework_5.py
@@ -1,47 +1,67 @@
-# Домашнее задание  №5
-
-import json
-import csv
 import re
+import csv
+import json
 
-debtors_data = []
-with open("traders.txt") as file:
-    for line in file:
-        debtors_data.append([x for x in line.split()])
+def make_traders_table(txt_intput_file_path, json_input_file_path, csv_output_file_path):
+    """
+    Функция, которая принимает в себя путь к файлу с ИНН и путь к файлу с информацией об организациях.
+    Возвращает словарь, в котором ключи - это ИНН, а значения - словари с ключами 'ogrn' и 'address'.
 
-for i in range(len(debtors_data)):
-    debtors_data[i] = debtors_data[i][0]
+    :param txt_intput_file_path: путь к файлу с ИНН
+    :param json_input_file_path: путь к файлу с информацией об организациях
+    :param csv_output_file_path: путь к файлу, в который будет записана таблица
+    :return: None
+    """
+    with open(txt_intput_file_path, "r", encoding='utf-8') as f:
+        inn = [line.strip() for line in f]
 
-with open("efrsb_1000.json") as f:
-    data_json = json.load(f)
+    traders_data = {}
+    with open(json_input_file_path, "r", encoding='utf-8') as f:
+        data = json.load(f)
+        for trader in data:
+            if trader["inn"] in inn:
+                traders_data[trader["inn"]] = {"ogrn": trader["ogrn"], "address": trader["address"]}
 
-listCSV = []
-for j in range(len(debtors_data)):
-    INN = debtors_data[j]
-    for i in range(len(data_json)):
+    with open(csv_output_file_path, "w", newline="", encoding='utf-8') as f:
+        writer = csv.writer(f)
+        for item in inn:
+            if item in traders_data:
+                writer.writerow([item, traders_data[item]["ogrn"], traders_data[item]["address"]])
 
-        if "debtor_inn" in data_json[i] and "debtor_ogrn" in data_json[i]: #and INN == data_json[i]['debtor_inn']
-            listCSV.append({'INN': data_json [i] ['debtor_inn'], 'OGRN': data_json[i]["debtor_ogrn"]})
+def extract_emails(text):
+    """
+    Функция, которая принимает в себя текст и возвращает список email-адресов, найденных в тексте.
+    :param text:
+    :return:
+    """
+    return re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', text)
 
-with open('traders.csv', 'w', newline='') as csvFile:
-    wr = csv.DictWriter(csvFile, delimiter = ';', fieldnames = ['INN', 'OGRN'])
-    wr.writeheader()
-    for i in listCSV:
-        wr.writerow(i)
+def extract_emails_from_json(json_input_file, json_output_file):
+    """
+    Функция, которая принимает в себя путь к файлу с информацией о сообщениях и путь к файлу, в который будет записан словарь.
+    :param json_input_file:
+    :param json_output_file:
+    :return:
+    """
+    with open(json_input_file, 'r', encoding='utf-8') as f:
+        messages = json.load(f)
+    emails_dict = {}
+    for message in messages:
+        inn = message['publisher_inn']
+        emails = extract_emails(message['msg_text'])
+        if emails:
+            if inn not in emails_dict:
+                emails_dict[inn] = set()
+            emails_dict[inn].update(emails)
 
-def find_emails(text):
-    lst = re.findall('\S+@\S+', text)
-    return lst
+    with open(json_output_file, 'w') as f:
+        json.dump({k: list(v) for k, v in emails_dict.items()}, f, ensure_ascii=False, indent=4)
 
-with open('efrsb.json') as file:
-    emails_list = json.load(file)
-
-dictionary_emails = {}
-
-for i in range(len(emails_list)):
-    emails_set = set(find_emails(emails_list[i]['msg_text']))
-    if emails_set != set():
-        dictionary_emails[str(emails_list[i]['publisher_inn'])] = emails_set
-
-with open('emails.json', 'w') as outfile:
-    json.dump(dictionary_emails, outfile, default=list)
+if __name__ == '__main__':
+    traders_txt = 'traders.txt'
+    traders_json = 'traders.json'
+    traders_csv = 'traders.csv'
+    messages_json = '1000_efrsb_messages.json'
+    emails_json = 'emails.json'
+    make_traders_table(traders_txt, traders_json, traders_csv)
+    extract_emails_from_json(messages_json, emails_json)
